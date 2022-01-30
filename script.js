8692887e270efb966765cfcdefa15d3724ce1be4
AkshayWarrier <aksmen121@gmail.com>
2022-01-30 11:59:34 +0530

    Add item implemented

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-var lists = document.querySelectorAll('.lists li')
-var no_lists = lists.length
-var addItem = document.querySelectorAll('.fa.fa-plus')
+var addList = document.querySelector('.add-list')
+console.log(addList)
+var addItem = document.querySelectorAll('.add-item .fa')
 
-function create_list(lists){
+function create_list(x){
+    var no_lists = document.querySelectorAll('.list-content').length
+    console.log(no_lists)
+    const list = document.createElement('li')
     no_lists += 1
-    const item = document.createElement('li')
-    item.className =   'l' + no_lists
-    item.appendChild('div')
-    item.firstChild.className = 'list-container'
+    list.className =   'l' + no_lists
+    list.appendChild('div')
+    list.children[0].className = 'list-container c'+no_lists
+    list.children[1].appendChild('p')
+    list.children[2].className = 'list-title' 
+    list.children[2].contentEditable = 'true'
+    list.children[2].innerText = 'New List'
+    list.children[2].appendChild('ul')
+    list.children[3].className = 'list-content'
+    list.children[3].appendChild('li')
+
 }
 function add_item(x){
     const item = document.createElement('li')
@@ -16,10 +26,10 @@ function add_item(x){
     item.innerText = 'New Item'
     path = x.path
     path[2].insertBefore(item,path[1])
-    // const list = document.querySelectorAll
-
 }
-
+addList.addEventListener("click",create_list)
 addItem.forEach(element => {
     element.addEventListener("click",add_item)
 });
+
+
