ca494d45456ae7241c22d2efb22aad255d4f2dbe
AkshayWarrier <aksmen121@gmail.com>
2022-02-10 09:38:50 +0530

    Add and remove list

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 var addList = document.querySelector('.add-list')
 var addItem = document.querySelectorAll('.add-item .fa')
+var removeItem = document.querySelectorAll('.remove-item')
+var removeList = document.querySelectorAll('.remove-list')
 
-colors_val = ["#da001d","#da5700","#dad600","#3eda00","#0095da","#1600da","#8a00da"]
-color_point = 3
+for(var i = 0; i < removeList.length; i++){
+    removeList[i].addEventListener('click', element => {
+        x = element.path[2].children
+        x[x.length-3].remove()
+    })
+}
+
+for(var i = 0; i < removeItem.length; i++){
+    removeItem[i].addEventListener('click',element => {
+        x = element.path[2].children
+        console.log(x[x.length - 3])
+        x[x.length-3].remove()
+    }
+    )
+}
+
 
 function create_list(x){
     var no_lists = document.querySelectorAll('.list-content').length
     no_lists += 1
 
     var list = document.createElement('li')
-    list.className =   'l' + no_lists
+    list.className =   'l'
 
     const list_container = document.createElement('div')
-    list_container.className = 'list-container c'+no_lists
-    if(color_point >= 7){
-        color_point = 0
-    }
-    list_container.style.border = "3px solid " + colors_val[color_point]
-    
+    list_container.className = 'list-container c'+no_lists  
     list.appendChild(list_container)
     
     const list_title = document.createElement('p')
@@ -31,8 +42,6 @@ function create_list(x){
     const item = document.createElement('li')
     item.className = 'item'
     item.innerText = "New Item"
-    item.style.backgroundColor = colors_val[color_point] + "94"
-    color_point += 1
     list_content.appendChild(item)
 
     const add_item_btn = document.createElement('li')
@@ -60,12 +69,10 @@ function add_item(x){
     item.className = 'item'
     item.contentEditable = 'true'
     item.innerText = 'New Item'
-    path = x.path
+    var path = x.path
     path[2].insertBefore(item,path[1])
 }
 addList.addEventListener("click",create_list)
 addItem.forEach(element => {
     element.addEventListener("click",add_item)
 });
-
-
