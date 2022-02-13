11141f6235613e68af30022d7850e14a6bd6d7e9
Akshay Warrier <aksmen121@gmail.com>
2022-02-13 18:13:14 +0530

    Draggable items

--- script.js
+++ script.js
@@ -31,7 +31,9 @@ function create_list(x){
     
     const list_title = document.createElement('p')
     list_title.className = 'list-title'
-    list_title.contentEditable = 'true'
+
+    list_title.setAttribute('onclick','this.contentEditable = true')
+    list_title.setAttribute('onblur','this.contentEditable = false')
     list_title.innerText = 'New List'
 
     const list_content = document.createElement('ul')
@@ -39,6 +41,8 @@ function create_list(x){
     
     const item = document.createElement('li')
     item.className = 'item'
+    item.setAttribute('onclick','this.contentEditable = true')
+    item.setAttribute('onblur','this.contentEditable = false')
     item.innerText = "New Item"
     list_content.appendChild(item)
 
@@ -78,6 +82,8 @@ function add_item(x){
     const item = document.createElement('li')
     item.className = 'item'
     item.contentEditable = 'true'
+    item.setAttribute('onclick','this.contentEditable = true')
+    item.setAttribute('onblur','this.contentEditable = false')
     item.innerText = 'New Item'
     var path = x.composedPath()
     path[2].insertBefore(item,path[1])
@@ -86,3 +92,45 @@ addList.addEventListener("click",create_list)
 addItem.forEach(element => {
     element.addEventListener("click",add_item)
 });
+
+const draggables = document.querySelectorAll('.item')
+const containers = document.querySelectorAll('.list-content')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container =>{
+    container.addEventListener('dragover',e => {
+        e.preventDefault()
+        const draggable = document.querySelector('.dragging')
+        const afterElement = getDragAfterElement(container,e.clientY)
+        console.log(afterElement)
+        if(afterElement == null){
+            container.appendChild(draggable)
+        }
+        else{
+            container.insertBefore(draggable,afterElement)
+        }
+    })
+} )
+
+function getDragAfterElement(container,y){
+    const draggableElements = [...container.querySelectorAll('.item:not(.dragging)')]
+    return draggableElements.reduce((closest,child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if(offset < 0 && offset > closest.offset){
+            return {offset:offset, element:child}
+        }else{
+            return closest
+        }
+    },{offset: Number.NEGATIVE_INFINITY}).element
+
+}
